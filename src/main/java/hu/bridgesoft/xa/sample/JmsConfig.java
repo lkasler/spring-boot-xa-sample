159233ef6b139211bc01123214168c3f629d4ab1
Laszlo Kasler <laszlo.kasler@gmail.com>
2019-05-05 20:04:55 +0200

    Add ActiveMQ atomikos related beans

---
+++ src/main/java/hu/bridgesoft/xa/sample/JmsConfig.java
@@ -0,0 +1,42 @@
+package hu.bridgesoft.xa.sample;
+
+import org.apache.activemq.ActiveMQXAConnectionFactory;
+import org.springframework.boot.jta.atomikos.AtomikosConnectionFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.jms.core.JmsTemplate;
+
+import javax.jms.Session;
+
+/**
+ * Jms atomikos config.
+ * @author kasler
+ */
+@Configuration
+@DependsOn("transactionManager")
+public class JmsConfig {
+
+    @Bean(name = "atomikosJmsConnectionFactor", initMethod = "init")
+    public AtomikosConnectionFactoryBean atomikosConnectionFactoryBean(){
+        AtomikosConnectionFactoryBean atomikosConnectionFactoryBean = new AtomikosConnectionFactoryBean();
+        atomikosConnectionFactoryBean.setUniqueResourceName("ActiveMQXA");
+        ActiveMQXAConnectionFactory activeMQXAConnectionFactory = new ActiveMQXAConnectionFactory();
+        activeMQXAConnectionFactory.setBrokerURL("tcp://localhost:61616");
+        activeMQXAConnectionFactory.setUserName("admin");
+        activeMQXAConnectionFactory.setPassword("admin");
+        atomikosConnectionFactoryBean.setXaConnectionFactory(activeMQXAConnectionFactory);
+        atomikosConnectionFactoryBean.setPoolSize(1);
+        return atomikosConnectionFactoryBean;
+    }
+
+    @Bean
+    public JmsTemplate jmsTemplate(){
+        JmsTemplate jmsTemplate = new JmsTemplate();
+        jmsTemplate.setConnectionFactory(atomikosConnectionFactoryBean());
+        jmsTemplate.setReceiveTimeout(2000);
+        jmsTemplate.setSessionTransacted(true);
+        jmsTemplate.setSessionAcknowledgeMode(Session.SESSION_TRANSACTED);
+        return jmsTemplate;
+    }
+}
--- src/main/java/hu/bridgesoft/xa/sample/MainConfig.java
+++ src/main/java/hu/bridgesoft/xa/sample/MainConfig.java
@@ -1,8 +1,7 @@
 package hu.bridgesoft.xa.sample;
 
-import javax.transaction.TransactionManager;
-import javax.transaction.UserTransaction;
-
+import com.atomikos.icatch.jta.UserTransactionImp;
+import com.atomikos.icatch.jta.UserTransactionManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -15,54 +14,54 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.jta.JtaTransactionManager;
 
-import com.atomikos.icatch.jta.UserTransactionImp;
-import com.atomikos.icatch.jta.UserTransactionManager;
+import javax.transaction.TransactionManager;
+import javax.transaction.UserTransaction;
 
 @Configuration
 @ComponentScan
 @EnableTransactionManagement
 public class MainConfig {
 
-	@Bean
-	public PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
-		return new PropertySourcesPlaceholderConfigurer();
-	}
-	
-	@Bean
-	public JpaVendorAdapter jpaVendorAdapter() {
-		HibernateJpaVendorAdapter hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
-		hibernateJpaVendorAdapter.setShowSql(true);
-		hibernateJpaVendorAdapter.setGenerateDdl(true);
-		hibernateJpaVendorAdapter.setDatabase(Database.MYSQL);
-		return hibernateJpaVendorAdapter;
-	}
+    @Bean
+    public PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
 
-	@Bean(name = "userTransaction")
-	public UserTransaction userTransaction() throws Throwable {
-		UserTransactionImp userTransactionImp = new UserTransactionImp();
-		userTransactionImp.setTransactionTimeout(10000);
-		return userTransactionImp;
-	}
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
+        hibernateJpaVendorAdapter.setShowSql(true);
+        hibernateJpaVendorAdapter.setGenerateDdl(true);
+        hibernateJpaVendorAdapter.setDatabase(Database.MYSQL);
+        return hibernateJpaVendorAdapter;
+    }
 
-	@Bean(name = "atomikosTransactionManager", initMethod = "init", destroyMethod = "close")
-	public TransactionManager atomikosTransactionManager() throws Throwable {
-		UserTransactionManager userTransactionManager = new UserTransactionManager();
-		userTransactionManager.setForceShutdown(false);
+    @Bean(name = "userTransaction")
+    public UserTransaction userTransaction() throws Throwable {
+        UserTransactionImp userTransactionImp = new UserTransactionImp();
+        userTransactionImp.setTransactionTimeout(10000);
+        return userTransactionImp;
+    }
 
-		AtomikosJtaPlatform.transactionManager = userTransactionManager;
+    @Bean(name = "atomikosTransactionManager", initMethod = "init", destroyMethod = "close")
+    public TransactionManager atomikosTransactionManager() throws Throwable {
+        UserTransactionManager userTransactionManager = new UserTransactionManager();
+        userTransactionManager.setForceShutdown(false);
 
-		return userTransactionManager;
-	}
+        AtomikosJtaPlatform.transactionManager = userTransactionManager;
 
-	@Bean(name = "transactionManager")
-	@DependsOn({ "userTransaction", "atomikosTransactionManager" })
-	public PlatformTransactionManager transactionManager() throws Throwable {
-		UserTransaction userTransaction = userTransaction();
+        return userTransactionManager;
+    }
 
-		AtomikosJtaPlatform.transaction = userTransaction;
+    @Bean(name = "transactionManager")
+    @DependsOn({"userTransaction", "atomikosTransactionManager"})
+    public PlatformTransactionManager transactionManager() throws Throwable {
+        UserTransaction userTransaction = userTransaction();
 
-		TransactionManager atomikosTransactionManager = atomikosTransactionManager();
-		return new JtaTransactionManager(userTransaction, atomikosTransactionManager);
-	}
+        AtomikosJtaPlatform.transaction = userTransaction;
+
+        TransactionManager atomikosTransactionManager = atomikosTransactionManager();
+        return new JtaTransactionManager(userTransaction, atomikosTransactionManager);
+    }
 
 }
